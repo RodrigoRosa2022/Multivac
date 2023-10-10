d932efd52d6ab5b011ea07c3459f0d8958b97edd
RodrigoRosa2022 <ronibfr@gmail.com>
2023-10-10 00:21:58 -0300

    first commit

---
+++ src/components/Terminal.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="screen">
+  <img src="@/assets/terminalzoom.jpg" alt="Screen Frame" width="400" class="terminal-image">
+  <p>teste</p>
+  <input type="text" class="input" @input="keyPress()">
+</div>
+</template>
+
+<script>
+export default {
+  methods: {
+    keyPress() {
+      console.log("Key Pressed")
+    }
+  }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.screen {
+  border: 3px solid white;
+  border-radius: 20px;
+  background: black;
+  margin: 40px;
+  width: 100%;
+  box-shadow: 0 0 10px black;
+}
+
+.terminal-image {
+  border-radius: 30px;
+}
+
+.input {
+  height: 50px;
+  width: 50px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,71 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="logo">
+      <h1>The Multivac</h1>
+      <img src="../assets/multivac.jpg" alt="Multivac Terminal" height="400">
+      <p>The Retrofuturistic Oracle with an Infinite Improbability Twist.</p>
+
+    </div>
+      <Terminal />
   </div>
+  <footer>
+    <p>Coded with sweat, blood and tears.<br>Original Website by Rodrigo Rosa</p>
+  </footer>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Terminal from '@/components/Terminal.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Terminal
   }
 }
 </script>
+
+<style scoped>
+.home {
+  display: flex;
+  /* flex direction: column; */
+  gap: 16px;
+  height: 80%;
+}
+
+h1 {
+  center: aligned;
+  font-size: 60px;
+}
+.logo {
+  border-right: 1px solid;
+  border-image: linear-gradient (red, white) 1;
+  width: 50%;
+  height: 100%;
+  text-align: center;
+}
+
+.logo p {
+  width: 230px;
+  margin: auto;
+  /* margin: 20px; */
+}
+
+.logo img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 60px;
+  /* width: 100%; */
+}
+
+.filler {
+  height: 30px;
+}
+
+footer {
+  text-align: center;
+  margin-top: 40px;
+}
+
+</style>
+
